85d7bb7d1fb9bfb86b4d012474318e2c1cacb262
KS9004 <uic.19mca8068@gmail.com>
2021-02-03 15:19:11 +0530

    neumorphic button,card created

--- lib/fitness.dart
+++ lib/fitness.dart
@@ -35,6 +35,7 @@ class Fitness extends StatelessWidget {
            ]
            ),
            child: Column(
+
              children: [
                Row(
                  mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -46,6 +47,122 @@ class Fitness extends StatelessWidget {
                  ),),
                    Icon(FontAwesomeIcons.user )
                ],),
+               SizedBox(height:40.0),
+               Row(
+                 mainAxisAlignment: MainAxisAlignment.spaceAround,
+                 children: [
+                   RichText(text: TextSpan(text: "Day 1 \n",
+                       style: TextStyle(color: Colors.black,fontSize: 50.0),
+                      children: [
+                        TextSpan(text: 'Strength',style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.black12,
+                          fontWeight: FontWeight.bold
+                        )),
+                      ]
+                   ),
+                   ),
+                   Container(
+                     width: 120.0,
+                     decoration: BoxDecoration(
+                       borderRadius: BorderRadius.circular(49.0),
+                       gradient: LinearGradient(
+                         begin: Alignment(-1.0, -4.0),
+                         end: Alignment(1.0, 4.0),
+                         colors: [
+                           Color(0xfff5ffff),
+                           Color(0xffced9e1)
+                         ]
+                       ),
+                       boxShadow: [
+                         BoxShadow(
+                           offset: Offset(15, 15),
+                           color: Color(0xffc3cdd5),
+                           blurRadius: 15,
+                           spreadRadius: 1.0
+                         ),
+                         BoxShadow(
+                             offset: Offset(-15, -15),
+                             color: Color(0xffffffff),
+                             blurRadius: 25,
+                             spreadRadius: 1.0,
+                         ),
+                       ]
+                     ),
+                     child: MaterialButton(
+                         onPressed: null,
+                     child: Text("Next",style: TextStyle(
+                       color: Colors.black,
+                       fontSize: 20.0
+                     ),),),
+                   )
+                 ],
+               ),
+               SizedBox(height: 50.0,),
+               Container(
+                 margin: EdgeInsets.symmetric(horizontal: 30.0),
+                 width: double.infinity,
+                 height: 160.0,
+                 decoration: BoxDecoration(
+                     borderRadius: BorderRadius.circular(
+                         20.0),
+                     gradient: LinearGradient(
+                         begin: Alignment(-1.0, -4.0),
+                         end: Alignment(1.0, 4.0),
+                         colors: [
+                           Color(0xfff5ffff),
+                           Color(0xffced9e1)
+                         ]
+                     ),
+                     boxShadow: [
+                       BoxShadow(
+                           offset: Offset(15, 15),
+                           color: Color(0xffc3cdd5),
+                           blurRadius: 15,
+                           spreadRadius: 1.0
+                       ),
+                       BoxShadow(
+                         offset: Offset(-15, -15),
+                         color: Color(0xffffffff),
+                         blurRadius: 25,
+                         spreadRadius: 1.0,
+                       ),
+                     ]
+                 ),
+                 child: MaterialButton()
+               ),
+               Container(
+                   margin: EdgeInsets.only(top: 50.0,left: 5.0,right: 5.0),
+                   width: double.infinity,
+                   height: 405.0,
+                   decoration: BoxDecoration(
+                       borderRadius: BorderRadius.circular(
+                           20.0),
+                       gradient: LinearGradient(
+                           begin: Alignment(-1.0, -4.0),
+                           end: Alignment(1.0, 4.0),
+                           colors: [
+                             Color(0xfff5ffff),
+                             Color(0xffced9e1)
+                           ]
+                       ),
+                       boxShadow: [
+                         BoxShadow(
+                             offset: Offset(15, 15),
+                             color: Color(0xffc3cdd5),
+                             blurRadius: 15,
+                             spreadRadius: 1.0
+                         ),
+                         BoxShadow(
+                           offset: Offset(-15, -15),
+                           color: Color(0xffffffff),
+                           blurRadius: 25,
+                           spreadRadius: 1.0,
+                         ),
+                       ]
+                   ),
+                   child: MaterialButton()
+               ),
              ],
            ),
          ),
