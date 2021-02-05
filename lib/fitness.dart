cdfb371c559ddb84e15bad8da853e13332ff718c
KS9004 <uic.19mca8068@gmail.com>
2021-02-05 15:03:04 +0530

    added video section for fitness

--- lib/fitness.dart
+++ lib/fitness.dart
@@ -1,7 +1,9 @@
+import 'package:fitness_ui_app/fitnessList.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Fitness extends StatelessWidget {
+var fit = FitnessData();
   @override
   Widget build(BuildContext context) {
     MediaQueryData deviceSize = MediaQuery.of(context);
@@ -229,7 +231,97 @@ class Fitness extends StatelessWidget {
                          ),
                        ]
                    ),
-                   child: MaterialButton()
+                   child:ListView.builder(
+                        itemCount: fit.fitnessList.length,
+                       itemBuilder: (BuildContext context,int index){
+                     return Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                       children: [
+                         Container(
+                           margin: EdgeInsets.only(left: deviceSize.size.width*0.03,top: deviceSize.size.height*0.05),
+                           width: deviceSize.size.width*0.3,
+                           height: deviceSize.size.height*0.13,
+                           decoration: BoxDecoration(
+                             image:DecorationImage(
+                               image: AssetImage(fit.fitnessList[index]['picture'],
+                               ),
+                               fit: BoxFit.cover
+                             ),
+                               borderRadius: BorderRadius.circular(20.0),
+                               gradient: LinearGradient(
+                                   begin: Alignment(-1.0, -4.0),
+                                   end: Alignment(1.0, 4.0),
+                                   colors: [
+                                     Color(0xfff5ffff),
+                                     Color(0xffced9e1)
+                                   ]
+                               ),
+                               boxShadow: [
+                                 BoxShadow(
+                                     offset: Offset(15, 15),
+                                     color: Color(0xffc3cdd5),
+                                     blurRadius: 15,
+                                     spreadRadius: 1.0
+                                 ),
+                                 BoxShadow(
+                                   offset: Offset(-15, -15),
+                                   color: Color(0xffffffff),
+                                   blurRadius: 25,
+                                   spreadRadius: 1.0,
+                                 ),
+                               ]
+                           ),
+                          
+                         ),
+                         SizedBox(width: deviceSize.size.width*0.01,),
+                         RichText(
+                           text: TextSpan(text: fit.fitnessList[index]['name'],
+                             style: TextStyle(color: Color(0xff62819D),fontSize: 20.0,
+                                 fontWeight: FontWeight.bold),
+                             children: [
+                               TextSpan(text: fit.fitnessList[index]['duration'],style: TextStyle(
+                                   fontSize: 20.0,
+                                   color: Colors.black12,
+                                   fontWeight: FontWeight.bold
+                               )),
+                             ]
+                         ),
+                         ),
+                         SizedBox(width: deviceSize.size.width*0.01,),
+                         Container(
+                           margin: EdgeInsets.only(top: deviceSize.size.height*0.05,right: deviceSize.size.width*0.03),
+                           width: deviceSize.size.width*0.2,
+                           height: deviceSize.size.height*0.08,
+                           decoration: BoxDecoration(
+                               borderRadius: BorderRadius.circular(15.0),
+                               gradient: LinearGradient(
+                                   begin: Alignment(-1.0, -4.0),
+                                   end: Alignment(1.0, 4.0),
+                                   colors: [
+                                     Color(0xfff5ffff),
+                                     Color(0xffced9e1)
+                                   ]
+                               ),
+                               boxShadow: [
+                                 BoxShadow(
+                                     offset: Offset(15, 15),
+                                     color: Color(0xffc3cdd5),
+                                     blurRadius: 15,
+                                     spreadRadius: 1.0
+                                 ),
+                                 BoxShadow(
+                                   offset: Offset(-15, -15),
+                                   color: Color(0xffffffff),
+                                   blurRadius: 25,
+                                   spreadRadius: 1.0,
+                                 ),
+                               ]
+                           ),
+                           child: IconButton(icon:Icon(Icons.play_arrow_rounded,size: 50.0,),),
+                         ),
+                       ],
+                     );
+                   }),
                ),
              ],
            ),
@@ -238,3 +330,5 @@ class Fitness extends StatelessWidget {
     );
   }
 }
+
+
---
+++ lib/fitnessList.dart
@@ -0,0 +1,15 @@
+class FitnessData{
+  List fitnessList =[
+    {
+      'name':"Walkout\n",
+      'picture':"assets/walout.jpg",
+      'duration':'0:40'
+    },
+
+    {
+      'name':"Deadlifts\n",
+      'picture':"assets/deadlifts.jpg",
+      'duration':'0:50'
+    }
+  ];
+}
